652717d0aadbaf96e2d460df3671e29039f89c89
IgnatZakalinsky <ai73a@yandex.ru>
2021-01-20 14:42:39 +0300

    change back url

--- src/baseBackURL.ts
+++ src/baseBackURL.ts
@@ -1,2 +1,2 @@
-// export const baseURL = 'http://31.131.28.206:8008/'
-export const baseURL = 'http://127.0.0.1:8000/'
+export const baseURL = 'http://31.131.28.206:8888/'
+// export const baseURL = 'http://127.0.0.1:8000/'
