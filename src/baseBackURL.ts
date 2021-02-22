5bbac885c57ce367c3196e87bb683c1bed292894
neko <ai73a@yandex.ru>
2021-02-22 14:22:05 +0300

    refactoring

--- src/baseBackURL.ts
+++ src/baseBackURL.ts
@@ -1,6 +1,6 @@
 //export const baseURL = process.env.REACT_APP_BASE_BACK_URL || 'http://31.131.28.206:8888/'
-const protocol = process.env.REACT_APP_PROTOCOL; 
-const host = process.env.REACT_APP_HOST ;
-const port = process.env.REACT_APP_PORT;
+const protocol = process.env.REACT_APP_PROTOCOL
+const host = process.env.REACT_APP_HOST
+const port = process.env.REACT_APP_PORT
 
-export const baseURL = protocol+"://"+host+":"+port
+export const baseURL = protocol + '://' + host + ':' + port
