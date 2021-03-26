61cd8a519a8ebf98c53bc058960396029a4a35f0
neko <ai73a@yandex.ru>
2021-03-26 13:53:16 +0300

    header refactoring

--- src/i1-main/m1-ui/u3-header/Header.tsx
+++ src/i1-main/m1-ui/u3-header/Header.tsx
@@ -4,45 +4,18 @@ import {useSelector} from 'react-redux'
 import s from './Header.module.css'
 import {AppStoreType} from '../../m2-bll/store'
 import {PATH} from '../u2-main/Main'
-// import Contacts from '../u6-contacts/Contacts'
-// import logo from './../../../assets/logo-ordit.png'
 import newLogo from './../../../assets/new-logo.svg'
-// import men from './../../../assets/men.png'
-// import trueMen from './../../../assets/true-men.png'
 import newMen from './../../../assets/new-man.svg'
 
-type HeaderPropsType = {
-    isAuth: boolean
-}
+type HeaderPropsType = {}
 
-const Header: React.FC<HeaderPropsType> = ({isAuth}) => {
+const Header: React.FC<HeaderPropsType> = () => {
     const {username} = useSelector((store: AppStoreType) => store.app.data) || {}
+    const {isAuth} = useSelector((store: AppStoreType) => store.app)
 
     return (
         <div className={s.header}>
-            <div className={s.start}>
-                <NavLink
-                    to={PATH.ACCOUNT}
-                    activeClassName={s.active + ' ' + s.item}
-                    className={s.link + ' ' + s.item}
-                >
-                    <span className={s.text}>кабинет</span>
-                </NavLink>
-                {/*<NavLink*/}
-                {/*    to={PATH.COURSES}*/}
-                {/*    activeClassName={s.active + ' ' + s.item}*/}
-                {/*    className={s.link + ' ' + s.item}*/}
-                {/*>*/}
-                {/*    <span className={s.text}>курсы</span>*/}
-                {/*</NavLink>*/}
-                <NavLink
-                    to={PATH.MAIL}
-                    activeClassName={s.active + ' ' + s.item}
-                    className={s.link + ' ' + s.item}
-                >
-                    <span className={s.text}>почта</span>
-                </NavLink>
-            </div>
+            <div style={{width: 210, height: 30}}/>
 
             <div className={s.center}>
                 <div className={s.text}>зимняя</div>
@@ -50,30 +23,24 @@ const Header: React.FC<HeaderPropsType> = ({isAuth}) => {
                 <div className={s.text}>орбита</div>
             </div>
 
-            <div className={s.end}>
-                <div>
-                    {/*    <Contacts/>*/}
-                </div>
+            <div className={s.person}>
+                {!isAuth
+                    ? (
+                        <>
+                            <NavLink className={s.text} to={PATH.LOGIN}>авторизация</NavLink>
+                            <img src={newMen} alt={'person'}/>
+                        </>
+                    ) : (
+                        <>
+                            <div className={s.text}>
+                                <div>добро пожаловать</div>
+                                <span>{username}</span>
+                            </div>
 
-                <div className={s.person}>
-                    {!isAuth
-                        ? (
-                            <>
-                                <NavLink className={s.text} to={PATH.LOGIN}>авторизация</NavLink>
-                                <img src={newMen} alt={'person'}/>
-                            </>
-                        ) : (
-                            <>
-                                <div className={s.text}>
-                                    <div>добро пожаловать</div>
-                                    <span>{username}</span>
-                                </div>
-
-                                <img src={newMen} alt={'trueMen'}/>
-                            </>
-                        )
-                    }
-                </div>
+                            <img src={newMen} alt={'trueMen'}/>
+                        </>
+                    )
+                }
             </div>
         </div>
     )
