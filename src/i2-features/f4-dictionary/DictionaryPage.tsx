eeb2aa88117920b54bb75a6501cbe1d6fcfcc321
neko <ai73a@yandex.ru>
2021-04-01 17:12:22 +0300

    add find logic

--- src/i2-features/f4-dictionary/DictionaryPage.tsx
+++ src/i2-features/f4-dictionary/DictionaryPage.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import PageWithNavbar from '../../i1-main/m1-ui/u6-navbar/PageWithNavbar'
 import s from './Dictionary.module.css'
+import find from './../../assets/find.svg'
 
 const DictionaryPage = () => {
+    const [word, setWord] = useState('')
+    const [wordF, setWordF] = useState('')
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setWord(e.currentTarget.value.trim())
+    }
+    const onClick = () => setWordF(word)
+
+    const content = [
+        {name: 'abc'},
+        {name: 'bcd'},
+        {name: 'abcd'},
+        {name: 'cd'},
+    ].filter(c => new RegExp(wordF, 'ig').test(c.name)).map(l => l.name)
+
     return (
         <PageWithNavbar>
             <div className={s.main}>
-                DictionaryPage
+                <div className={s.find}>
+                    <div>
+                        <img src={find} alt={'find'} className={s.img}/>
+                        <input placeholder={'введите слово'} value={word} onChange={onChange} className={s.input}/>
+                    </div>
+
+                    <div className={s.line}/>
+
+                    <button className={s.button} onClick={onClick}>найти</button>
+                </div>
+
+                {content}
             </div>
         </PageWithNavbar>
     )
